eae94fd15015145cfe0a152c079692f63630feca
wlanboy <me@wlanboy.com>
2019-04-05 11:51:04 +0200

    Added Spring Boot application

---
+++ src/main/java/com/wlanboy/cloudconfigpropertygenerator/CloudconfigpropertygeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.wlanboy.cloudconfigpropertygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication()
+public class CloudconfigpropertygeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconfigpropertygeneratorApplication.class, args);
+	}
+}
