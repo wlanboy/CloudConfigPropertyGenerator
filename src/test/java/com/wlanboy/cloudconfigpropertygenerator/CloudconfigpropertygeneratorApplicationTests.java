2f3fde23148124caee9aa9c35f21429ced7b44be
wlanboy <wlanboy@github.com>
2026-05-01 13:45:55 +0200

    added test env vars

--- src/test/java/com/wlanboy/cloudconfigpropertygenerator/CloudconfigpropertygeneratorApplicationTests.java
+++ src/test/java/com/wlanboy/cloudconfigpropertygenerator/CloudconfigpropertygeneratorApplicationTests.java
@@ -3,7 +3,12 @@ package com.wlanboy.cloudconfigpropertygenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "CONFIGMAP_FILE_TO_WRITE=./output/configmap.yaml")
+@SpringBootTest(properties = {
+		"CONFIGMAP_FILE_TO_WRITE=./output/configmap.yaml",
+		"SPRING_CLOUD_CONFIG_URI=http://localhost:8888",
+		"SPRING_PROFILES_ACTIVE=default",
+		"SPRING_APPLICATION_NAME=cloudconfigpropertygenerator"
+})
 public class CloudconfigpropertygeneratorApplicationTests {
 
 	@Test
